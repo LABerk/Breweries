06c4f678bb647eba0752533393e39b537660ca5c
Laura Berkey <berkeylaura@gmail.com>
2022-10-27 13:17:39 -0500

    links clickable

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -2,14 +2,16 @@ import React from "react";
 
 export const Brewery = (props) => {
   return (
-    <li>
+    <div>
       <h2>{props.name}</h2>
       <h3>{props.type}</h3>
       <h3>{props.street}</h3>
       <h3>{props.city}</h3>
       <h3>{props.state}</h3>
       <h3>{props.zip}</h3>
-      <h3>{props.website}</h3>
-    </li>
+      <a href={props.website} target="_blank" rel="noreferrer">
+        {props.website}
+      </a>
+    </div>
   );
 };
