6e83ac71eebf42c5de96af50abeab1173a9a399b
Laura Berkey <berkeylaura@gmail.com>
2022-10-28 20:25:09 -0500

    basic routing

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const fs = require("fs");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const routes = require("./routes/routes.js")(app, fs);
 
 app.get("/", (req, res) => {
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
